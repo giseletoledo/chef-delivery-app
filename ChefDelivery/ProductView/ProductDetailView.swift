800d01944e477f3773b6379099b69411c62c7713
gisele <gisesonia@gmail.com>
2023-07-28 21:59:40 -0300

    binding da quantidade na productview

--- ChefDelivery/ProductView/ProductDetailView.swift
+++ ChefDelivery/ProductView/ProductDetailView.swift
@@ -11,90 +11,48 @@ struct ProductDetailView: View {
     
     let product: ProductType
     @State private var productQuantity = 1
+
     
     var body: some View {
         VStack {
-            VStack(alignment: .leading, spacing: 16) {
-                Image(product.image)
-                    .resizable()
-                    .scaledToFit()
-                    .shadow(radius: 20)
-                
-                Text(product.name)
-                    .font(.title)
-                    .bold()
-                    .padding(.horizontal)
-                    .padding(.top)
-                
-                Text(product.description)
-                    .padding(.horizontal)
-                
-                Text(product.formattedPrice)
-                    .font(.title3)
-                    .bold()
-                    .padding(.horizontal)
-            }
+            
+            ProductDetailHeaderView(product: product)
             
             Spacer()
             
-            VStack{
-                Text("Quantidade")
-                    .font(.title3)
-                    .bold()
-                
-                HStack {
-                    Button {
-                        if productQuantity > 1 {
-                            productQuantity -= 1
-                            
-                        }
-                    } label: {
-                        Image(systemName: "minus.circle.fill")
-                            .font(.title)
-                            .bold()
-                    }
-                    
-                    Text("\(productQuantity)")
-                        .font(.title2)
-                        .bold()
-                    
-                    Button {
-                        productQuantity += 1                    } label: {
-                        Image(systemName: "plus.circle.fill")
-                            .font(.title)
-                            .bold()
-                    }
-                }
-            }
+           ProductDetailQuantityView(productQuantity: $productQuantity)
             
             Spacer()
             
-            Button {
-                print("O botão foi pressionado")
-            } label: {
-                HStack{
-                    Image(systemName: "cart")
-                    
-                    Text("Adicionar ao carrinho")
-                }
-                .padding(.horizontal, 32)
-                .padding(.vertical, 16)
-                .font(.title3)
-                .bold()
-                .background(Color("ColorRed"))
-                .foregroundColor(.white)
-                .cornerRadius(32)
-                .shadow(color: Color("ColorRed").opacity(0.5), radius: 10, x: 6, y: 8)
-            }
-
+            ProductDetailButtonView()
         }
-        
-        
     }
 }
 
 struct ProductDetailView_Previews: PreviewProvider {
     static var previews: some View {
         ProductDetailView(product: storesMock[0].products[0])
+    }
+}
+
+struct ProductDetailButtonView: View {
+    var body: some View {
+        Button {
+            print("O botão foi pressionado")
+        } label: {
+            HStack{
+                Image(systemName: "cart")
+                
+                Text("Adicionar ao carrinho")
+            }
+            .padding(.horizontal, 32)
+            .padding(.vertical, 16)
+            .font(.title3)
+            .bold()
+            .background(Color("ColorRed"))
+            .foregroundColor(.white)
+            .cornerRadius(32)
+            .shadow(color: Color("ColorRed").opacity(0.5), radius: 10, x: 6, y: 8)
+        }
     }
 }
